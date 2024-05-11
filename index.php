f42c90c90ddf7a8e77aeec7e7eea0de9a4ae6aea
JaimeYague <jaime.yague.matias@bootcamp-upgrade.com>
2024-05-11 11:55:09 +0200

    muestro listado de tweets

--- index.php
+++ index.php
@@ -1,7 +1,22 @@
 <?php
 
-$texto = "En un lugar de la Mancha"; //1.declara variables
-$autor = "Miguel de Cervantes";
-$user = "elMancoDeLepanto";
+
+$tweets = [
+    [
+        "texto" => "En un lugar de la Mancha",
+        "autor" => "Miguel de Cervantes",
+        "user" => "elMancoDeLepanto"
+    ],
+    [
+        "texto" => "El señor de los anillos",
+        "autor" => "Tolkien",
+        "user" => "Maria"
+    ],
+    [
+        "texto" => "La llamada de Cthulhu",
+        "autor" => "Lovecraft",
+        "user" => "Jaime"
+    ],
+];
 
 require_once "index.html"; //2.declaro el require

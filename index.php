321daf6842e0ce2191a90fa0f9d3cf06364afc85
estefaniaestepa <Gabriel301092/+>
2024-05-11 11:27:06 +0200

    primercommit del ejercicio de twitter

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+
+$texto = "En un lugar de la Mancha"; //1.declara variables
+$autor = "Miguel de Cervantes";
+$user = "elMancoDeLepanto";
+
+require_once "index.html"; //2.declaro el require
